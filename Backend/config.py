b126f47eae646cebc690e711d39e8149c6cf3568
shradha810 <60320258+shradha810@users.noreply.github.com>
2023-12-03 13:23:22 +0530

    Update config.py

--- Backend/config.py
+++ Backend/config.py
@@ -1,1 +1,1 @@
-MONGO='mongodb+srv://shradhaagarwal810:<PAssword>@shradhalearn.nmfq8ks.mongodb.net/?retryWrites=true&w=majority'
+MONGO='mongodb+srv://shradhaagarwal810:<Password>@shradhalearn.nmfq8ks.mongodb.net/?retryWrites=true&w=majority'
